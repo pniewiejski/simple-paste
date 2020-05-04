0e182cfbc5f134f479c4ed21a45776ed8b21f80f
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-05-04 19:37:19 +0200

    GUI - Add material UI theme definition

---
+++ simple-paste-client/simple-paste/src/theme.js
@@ -0,0 +1,18 @@
+const themeDefinition = {
+  palette: {
+    primary: {
+      light: '#efefef',
+      main: '#616161',
+      dark: '#8d8d8d',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffa06d',
+      main: '#ff6e40',
+      dark: '#c53d13',
+      contrastText: '#000',
+    },
+  },
+}
+
+export default themeDefinition
