51a961f997968c7a06ea9e28e9f159b13e4d22a2
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-05-17 21:26:29 +0200

    GUI - Configure the development proxy for backend api calls
    To make valid requests to the backend we need to add X-Simple-Paste header.
    The create-react-app 'proxy' entry in the package.json is not enough to
    do it. This is why a proxy server is configured using
    `http-proxy-middleware`.
    
    It is also necessary to cut the `/api` fragment from url. ✂️
    
    See create-react-app docs for more info:
    https://create-react-app.dev/docs/proxying-api-requests-in-development/

---
+++ simple-paste-client/simple-paste/src/setupProxy.js
@@ -0,0 +1,13 @@
+const {createProxyMiddleware} = require('http-proxy-middleware')
+
+module.exports = (app) => {
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'http://localhost:3001',
+      pathRewrite: {'^/api': ''},
+      headers: {
+        'X-Simple-Paste': 'dev-proxy',
+      }
+    })
+  )
+}
