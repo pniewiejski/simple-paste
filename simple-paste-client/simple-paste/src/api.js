b4df54cf77cde374ba4eb11deaba3cbbc150b8ec
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-05-24 19:40:49 +0200

    GUI - Fix linter errors 🤦
    It turns out that npm script used to run linter did not catch all files.
    It omited some files in the `./src/` directory. This is fixed by
    wrapping relative path in quotes when calling eslint.

--- simple-paste-client/simple-paste/src/api.js
+++ simple-paste-client/simple-paste/src/api.js
@@ -2,15 +2,18 @@ const API_ROOT = '/api' // Same origin proxy
 
 // TODO: Add error handling
 const requests = {
-  get: url => fetch(`${API_ROOT}${url}`),
-  post: (url, data) => fetch(`${API_ROOT}${url}`, {
-    method: 'POST', 
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data),
-  }),
+  get: (url) => fetch(`${API_ROOT}${url}`),
+  post: (url, data) =>
+    fetch(`${API_ROOT}${url}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    }),
 }
 
+// eslint-disable-next-line import/prefer-default-export
 export const Paste = {
-  getById: resourceId => requests.get(`/paste/${resourceId}`).then(response => response.json()),
-  create: pasteData => requests.post('/paste', pasteData)
+  getById: (resourceId) =>
+    requests.get(`/paste/${resourceId}`).then((response) => response.json()),
+  create: (pasteData) => requests.post('/paste', pasteData),
 }
--- simple-paste-client/simple-paste/src/constants.js
+++ simple-paste-client/simple-paste/src/constants.js
@@ -1,1 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
 export const PASTE_PERSISTANCE_OPTIONS = [5, 15, 30, 60]
